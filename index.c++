7df1d729f0e81fad38774a51b7e455bddf168087
meetamna8 <140243713+meetamna8@users.noreply.github.com>
2025-02-14 23:50:09 +0500

    Add files via upload

---
+++ index.c++
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+string name = "amna";
+name = "ayesha";
+cout<<name;
+
+
+return 0;
+}
---
+++ index.py
@@ -0,0 +1,24 @@
+# print("hello world")
+# name = "name"
+# age = 20
+# hello = True
+# bol = None
+# print(name.capitalize())
+# print(type(age))
+# print(type(hello))
+# print(type(bol))
+
+# a = 20
+# b = 8
+# print(a+b)
+
+
+# name = input("Enter your name : ")
+# print("welcome to our salon "+ name)
+
+# a = int(input("enter 1st no: "))
+# b = int(input("enter 2nd no: "))
+# sum = a+b
+# print(sum)
+name = input("enter your name: ")
+print("my name length is:", len(name))
