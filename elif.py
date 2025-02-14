24280f9084d2e702fc30ef42c2f6fb2c0cc585d7
meetamna8 <140243713+meetamna8@users.noreply.github.com>
2025-02-14 23:14:59 +0500

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,55 @@
+
+
+# while(True):
+#     color = input("enter color : ")
+#     if(color == "r"):
+#         print("stop")
+#     elif(color == "y"):
+#         print("get ready")
+#     elif(color == "g"):
+#         print("start")
+#     elif(color == "q"):
+#         break
+#     else:
+#         print("invalid input")
+
+# program#1
+# num = int(input("enter a num to check it is even or odd: "))
+# if(num%2 == 0):
+#     print("even")
+# else:
+#     print("odd")
+
+
+# program#3
+
+# num = int(input("enter a num to check it is multiple of 7 or not: "))
+# if(num%7 == 0):
+#     print("Multiple")
+# else:
+#     print("not a multiple")
+
+
+# program#3
+
+a = int(input("enter 1st number: "))
+b = int(input("enter 2nd number: "))
+c = int(input("enter 3rd number: "))
+
+if(a>b and a>c):
+    print("a is larger")
+elif(b>a and b>c):
+    print("b is larger")
+else:
+    print("c is larger")
+
+
+
+
+
+
+
+
+
+
+
